e527f84d7ee2c6d0a808c53498b35a12b186cae0
Gabriel Eric <gabriel.eric@ufrpe.br>
2026-05-31 20:09:15 -0300

    feat: add base layout template for iDiva's Portal with responsive design and AOS integration

--- routes/inews.py
+++ routes/inews.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
+import os
 
 inews_bp = Blueprint('inews', __name__, url_prefix='/inews')
 
 @inews_bp.route('/')
-def inews_index():
-    return render_template('pages/inews/index.html')
+def inews():
+    pasta_jornal = os.path.join(
+        current_app.static_folder,
+        'media',
+        'jornal'
+    )
+
+    try:
+        imagens_jornal = [
+            img for img in os.listdir(pasta_jornal)
+            if img.lower().endswith(('.png', '.jpg', '.jpeg'))
+        ]
+    except FileNotFoundError:
+        print("Erro: A pasta static/media/jornal não foi encontrada!")
+        imagens_jornal = []
+
+    return render_template(
+        'pages/inews/index.html',
+        imagens_jornal=imagens_jornal
+    )
